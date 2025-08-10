36471eedacea15de30932d78caf8bdfea83b3eb5
Gabriel Cavalante <132387174+GabsCavalcant@users.noreply.github.com>
2025-08-09 21:47:42 -0300

    Exercicio 1
    Crie um novo projeto Java Web no NetBeans, com o nome de “Contador”. Nesse projeto você deve criar um Servlet manualmente e dentro do método
    processRequest(...) use uma estrutura de repetição para direcionar para a saída
    padrão os números de 1 a 30.

---
+++ Contador/src/java/SevletContador.java
@@ -0,0 +1,35 @@
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author gabri
+ */
+@WebServlet(name = "SevletContador", urlPatterns = {"/Contador"})
+public class SevletContador extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp) {
+        for (int i = 0; i <= 30; i++) {
+            System.out.println(i);
+        }
+    }
+}
